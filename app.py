e272445059b65b4de1d82d59562bf1bbf605d1c2
swasher <mr.swasher@gmail.com>
2019-05-11 23:52:05 +0300

    commit13

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ app = Flask(__name__, static_folder='dist/static')
 # enable CORS
 CORS(app)
 
-import os
 
 @app.route('/json')
 def get_os_name():
     p = platform.platform()
     return jsonify({'platform': p})
 
+
 @app.route('/')
 def index():
-    dist_dir = '/app/dist'
-    entry = os.path.join(dist_dir, 'index.html')
-    return send_file(entry)
+    index_html = '/app/dist/index.html'
+    return send_file(index_html)

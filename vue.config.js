2d60cb53ed71bf844a72bfe9d2b04d8a85358cdf
swasher <mr.swasher@gmail.com>
2019-05-12 01:53:05 +0300

    commit21

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 module.exports = {
   assetsDir: 'static',
+  publicPath: IS_PRODUCTION
+    ? 'https://test654888.herokuapp.com/'
+    : '/',
 };

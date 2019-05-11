390dce29c677312f1bb0ec76a0aee6a3d3c2b596
swasher <mr.swasher@gmail.com>
2019-05-11 18:55:13 +0300

    init5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
 import platform
 
@@ -7,7 +7,14 @@ app = Flask(__name__)
 # enable CORS
 CORS(app)
 
+import os
+
 @app.route('/')
 def get_os_name():
-    p = platform.platform()
-    return jsonify({'platform': p})
+    # p = platform.platform()
+    # return jsonify({'platform': p})
+    APP_DIR = os.path.dirname(__file__)
+    ROOT_DIR = os.path.dirname(APP_DIR)
+    dist_dir = os.path.join(ROOT_DIR, 'dist')
+    entry = os.path.join(dist_dir, 'index.html')
+    return send_file(entry)

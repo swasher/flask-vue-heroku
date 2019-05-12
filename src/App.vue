b9e56624e2ede3bd52ccb9c54d7d59fedaf89da2
swasher <mr.swasher@gmail.com>
2019-05-12 16:38:05 +0300

    commit37

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <img alt="Vue logo" src="/jpg.jpg">
     <Os/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-
 module.exports = {
   assetsDir: 'static',
   outputDir: 'dist',
-  // publicPath: IS_PRODUCTION
-  //   ? 'https://test654888.herokuapp.com/'
-  //   : '/',
 };
